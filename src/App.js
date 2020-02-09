cd06a3c3964a8cfa27b24591a5659f5d98254406
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-09 08:13:19 +0700

    menambah data aktivitas harian

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state =  { todos: []  } 
+  }
+
+  addTodo = (e) => {
+    e.preventDefault();
+
+    let jam       = this.refs.jam.value;
+    let aktivitas = this.refs.aktivitas.value;
+
+    this.state.todos.push({jam, aktivitas});
+    this.setState({todos: this.state.todos})
+
+    this.refs.formulir.reset();
+    this.refs.jam.focus();
+  }
+
+
   render() {
     return (
       <div>
@@ -12,6 +31,18 @@ class App extends Component {
           <input type="text" ref="aktivitas" placeholder="jenis aktivitas"></input>
           <button onClick={this.addTodo}>simpan</button>
         </form>
+        <hr />
+        <div>
+        <ul>
+          { this.state.todos.map((data, i) =>
+            <li key={i}>
+              <div>
+                <button onClick={ ()=>this.removeTodo(i) }>hapus</button>{data.jam} : {data.aktivitas}
+              </div>
+            </li>
+          )}
+        </ul>
+        </div>
       </div>
     )
   }
