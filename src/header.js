929eba4ae52818588430e46f3dbce434539eccd5
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-09 09:00:43 +0700

    add fontawesome icon

--- src/header.js
+++ src/header.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Moment from 'moment';
+
 class Header extends Component {
     render() {
         let indonesia = require('moment/locale/id');
         Moment.updateLocale('id', indonesia);
 
         return(
+            <p align="center">
             <div>
+            <i class="fa fa-coffee"></i>
                 <h3>Aplikasi Aktivitas Harian</h3>
                     <p>{Moment().format('dddd')} {Moment().format('LLL')}</p>
             </div>
+            </p>
         );
     }
 }
